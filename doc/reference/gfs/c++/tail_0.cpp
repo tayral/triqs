1eb7bf9a7e3b73edeedd9c65d8d1519a7c75bc56
tayral <thomas.ayral@polytechnique.edu>
2016-11-21 16:13:38 -0500

    [doc] fix tail doc example
    mask deprecated --> reset()

--- doc/reference/gfs/c++/tail_0.cpp
+++ doc/reference/gfs/c++/tail_0.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 int main() {
  int N1 = 1, N2 = 1;
- triqs::gfs::tail t(N1, N2);
- t.mask()() = 5; // only coeffs from -1 to 5 are meaningful
+ triqs::gfs::tail t(triqs::arrays::make_shape(N1, N2));
+ t.reset(6); // only coeffs from -1 to 5 are meaningful
  std::cout << t(0) << std::endl;
  t(2) = .5;
  std::cout << t << std::endl;
